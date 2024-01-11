d4b586e67dc0be6f236e7b8a0adc0600c5289089
porcsiga <smnkamilla@gmail.com>
2024-01-11 14:38:56 +0100

    finish question posting page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Add from './Pages/Add.jsx'
 import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import Layout from './Pages/Layout.jsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <App />
+      },
+      {
+        path: "/add",
+        element: <Add />
+      }
+    ]
   },
 ]);
 
